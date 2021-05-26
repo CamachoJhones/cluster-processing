a1d8393d94644be7ca4c4855214788cf88119772
CamachoJhones <alonso.ccamacho@gmail.com>
2021-05-26 13:50:20 -0500

    New changes

--- master.py
+++ master.py
@@ -6,13 +6,11 @@ import threading
 #subprocess.call(['./servidor.py', 'enviar.txt'])
 #subprocess.call(['./cliente.py'])
 
-
 def sendFrame(filename):
     print(filename + "emvboemdp")
     ruta= "python servidor.py " + str(filename) +" 8001"
     print("RUTA +++++++++++++++ "+ ruta)
     os.system(ruta)
-
 def processFrames():
     for filename in os.listdir('./serverFrames'):
         t=threading.Thread(target=rutina)
@@ -21,9 +19,6 @@ def processFrames():
 
 def rutina():
     os.system("python cliente.py 8001")
-
-
-
 
 os.system("python servidor.py video.mp4 8000")
 os.system("python video2frames.py")

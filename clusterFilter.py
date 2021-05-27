2b00b07c3c36d0df85b362a47861a77a403ba351
emiliosg <3miliosanchez8@gmail.com>
2021-05-26 20:48:19 -0500

    Adapt to new file name

--- clusterFilter.py
+++ clusterFilter.py
@@ -1,8 +1,12 @@
+import sys
 import cv2 as cv
 cluster_number = str(1)
-frame_name = "frame22.png"
-frame_number = str(frame_name[5:].replace('.png',''))
+#frame_name = "frame22.png"
+frame_name = "from_server_frame22.png"
+frame_name = sys.argv[1]
+frame_number = str(frame_name[17:].replace('.png',''))
 img = cv.imread(frame_name,cv.IMREAD_GRAYSCALE)
+#cambio de directorio
 renderFrame = "frame"+frame_number+"-"+"ok-"+cluster_number+".png"
 cv.imwrite(renderFrame,img)
 print(renderFrame+" ready")
